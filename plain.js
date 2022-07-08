2dbfabf5ad6b3f5cd776004d6cc1ab1d91a9a5fa
JTkasenda <jeantshimbalanga9@gmail.com>
2022-07-08 10:26:35 +0100

    commit successful

--- plain.js
+++ plain.js
@@ -15,3 +15,6 @@ console.log(object.name)
 console.log(object.middlename)
 console.log(object.vueName())
 console.log(object.surname)
+
+
+//This is a test of all the things i've been learning recently, it is essential that I learn git
